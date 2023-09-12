1c9e9ffebf4364e7b9dfa1d9b5bc856779f4d56a
Георги Йорданов <gyordanov94@abv.bg>
2023-09-13 01:53:09 +0300

    add logout view and url. Add is user authenticated signs in the header in the template

--- BlogWebsite/BlogWebsite/account/urls.py
+++ BlogWebsite/BlogWebsite/account/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from BlogWebsite.account.views import registration_view
+from BlogWebsite.account.views import registration_view, logout_view
 
 urlpatterns = (
     path('register/', registration_view, name='register'),
+    path('logout/', logout_view, name='logout'),
 )
