7da1d71c4b8a9e301a7e8c6f6f11a1248771c8d1
Георги Йорданов <gyordanov94@abv.bg>
2023-09-13 01:04:39 +0300

    change user visualization in admin panel

--- BlogWebsite/BlogWebsite/account/admin.py
+++ BlogWebsite/BlogWebsite/account/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import AppUser
 
-# Register your models here.
+
+@admin.register(AppUser)
+class AccountAdmin(UserAdmin):
+    list_display = ('pk', 'email', 'username', 'date_joined', 'last_login', 'is_admin', 'is_staff')
+    search_fields = ('pk', 'email', 'username',)
+    # fields that will be read only and should never be changed
+    readonly_fields = ('pk', 'date_joined', 'last_login')
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+# admin.site.register(AppUser, AccountAdmin)
